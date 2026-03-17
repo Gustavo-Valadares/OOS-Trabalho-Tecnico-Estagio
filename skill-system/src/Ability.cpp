0bb7833ce4e62c2482d70add9f01a4da3bfa6a8f
Gustavo-Valadares <gustavoh.valadares@outlook.com>
2026-03-17 11:32:06 -0300

    feat: Criando o começo das classes

---
+++ skill-system/src/Ability.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Ability {
+    private:
+        string name;
+        int manaCost;
+        int coolDownValue;
+        int currentCoolDown;
+
+    
+};
---
+++ skill-system/src/Character.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Ability.hpp"
+
+using namespace std;
+
+class Player {
+    private:
+        string name;
+        int hp;
+        int mana;
+        int protection;
+
+    void useAbility(Ability ability){
+
+    }
+};
